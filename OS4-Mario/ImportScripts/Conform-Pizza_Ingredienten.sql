8ea40bdd627751e45676e7439815f2898df9367c
Seraphix <melvinkusters0@gmail.com>
2020-02-22 21:04:04 +0100

    [SQL] Added scripts for data import[Source:Pizza_ingredienten.xlsx].

---
+++ OS4-Mario/ImportScripts/Conform-Pizza_Ingredienten.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO [dbo].[Category] (
+	Name
+)
+SELECT DISTINCT categorie
+FROM [dbo].[pizza_ingredienten];
+GO
+
+-- Maar dan...
---
+++ OS4-Mario/ImportScripts/Import-Pizza_ingredienten.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Sheet1$] (
+	[categorie] nvarchar(255) NOT NULL,
+	[subcategorie] nvarchar(255) NOT NULL,
+	[productnaam] nvarchar(255) NOT NULL,
+	[productomschrijving] nvarchar(255) NOT NULL,
+	[prijs] money NOT NULL,
+	[bezorgtoeslag] money NOT NULL,
+	[spicy] nvarchar(255) NOT NULL,
+	[vegetarisch] nvarchar(255) NOT NULL,
+	[beschikbaar] nvarchar(255) NOT NULL,
+	[aantalkeer_ingredient] float NOT NULL,
+	[ingredientnaam] nvarchar(255) NOT NULL,
+	[pizzasaus_standaard] nvarchar(255) NOT NULL
+);
+GO
