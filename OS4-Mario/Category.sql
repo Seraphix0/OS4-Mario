30055db6ce3747f4f6d37dc0887e2cdaae50de97
Seraphix <melvinkusters0@gmail.com>
2020-02-22 21:04:29 +0100

    [SQL] Category hotfix SubCategoryId --> ParentCategoryId.

--- OS4-Mario/Category.sql
+++ OS4-Mario/Category.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Category]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NULL, 
-    [SubCategoryId] INT NULL, 
-    CONSTRAINT [FK_Category_SubCategory] FOREIGN KEY ([SubCategoryId]) REFERENCES [Category]([Id])
+    [Name] VARCHAR(50) NOT NULL, 
+    [ParentCategoryId] INT NULL, 
+    CONSTRAINT [FK_Category_ParentCategory] FOREIGN KEY ([ParentCategoryId]) REFERENCES [Category]([Id])
 )
